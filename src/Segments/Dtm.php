7ae25b7635728ea85d7d9e5295f95616f978366f
Nils <n.refle@die-strommixer.de>
2016-06-20 16:18:54 +0200

    DateQualifier format fixed

--- src/Segments/Dtm.php
+++ src/Segments/Dtm.php
@@ -25,7 +25,7 @@ class Dtm extends AbstractSegment
     {
         switch ($code) {
             case 102:
-                return $date->format('YmdH');
+                return $date->format('Ymd');
             case 203:
                return $date->format('YmdHi');
             case 303: 
@@ -41,7 +41,7 @@ class Dtm extends AbstractSegment
     {
         switch ($code) {
             case 102:
-                return DateTime::createFromFormat('YmdH', $string);
+                return DateTime::createFromFormat('Ymd', $string);
             case 203:
                 return DateTime::createFromFormat('YmdHi', $string);
             case 303: 
