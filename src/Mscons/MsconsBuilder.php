9882abd0c3330875d56506c060525e65de36ea03
Nils Refle <n.refle@proengeno.de>
2016-09-27 10:11:28 +0200

    Formatsumstellung

--- src/Mscons/MsconsBuilder.php
+++ src/Mscons/MsconsBuilder.php
@@ -7,5 +7,5 @@ use Proengeno\EdiEnergy\EdifactBuilder;
 abstract class MsconsBuilder extends EdifactBuilder
 {
     const MESSAGE_TYPE = 'MSCONS';
-    const ORGANISATION_CODE = '2.2f';
+    const ORGANISATION_CODE = '2.2g';
 }
