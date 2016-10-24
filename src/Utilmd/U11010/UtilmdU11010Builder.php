4d2ac5159bb341abaf7ba5becdd53368ed103a5b
Nils Refle <n.refle@proengeno.de>
2016-10-24 13:06:36 +0200

    UtilmdU11010 geaddet

---
+++ src/Utilmd/U11010/UtilmdU11010Builder.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Proengeno\EdiEnergy\Utilmd\U11010;
+
+use DateTime;
+use Proengeno\EdiEnergy\Utilmd\UtilmdBuilder;
+
+class UtilmdU11010Builder extends UtilmdBuilder
+{
+    const CHECK_DIGIT = 11010;
+
+    protected $edifactClass = UtilmdU11010::class;
+
+    protected function writeMessage($items)
+    {
+        $this->writeSeg('Unh', [
+            $this->unbReference(),
+            self::MESSAGE_TYPE,
+            self::VERSION_NUMBER,
+            self::RELEASE_NUMBER,
+            self::ORGANISATION,
+            self::ORGANISATION_CODE
+        ]);
+        $this->writeSeg('Bgm', ['E01', $this->unbReference()]);
+        $this->writeSeg('Dtm', [137, new DateTime, 203]);
+        $this->writeSeg('Nad', ['MS', $this->from, $this->getNadQualifier($this->from)], 'fromMpCode');
+        $this->writeSeg('Cta', ['IC', 'Herr Geerdes']);
+        $this->writeSeg('Com', ['04958 91570-10', 'TE']);
+        $this->writeSeg('Com', ['j.geerdes@proengeno.de', 'EM']);
+        $this->writeSeg('Nad', ['MR', $this->to, $this->getNadQualifier($this->to)], 'fromMpCode');
+
+        foreach ($items as $item) {
+            $this->writeItem($item);
+        }
+
+        $this->writeSeg('Unt', [$this->unhCount() + 1, $this->unbReference()]);
+    }
+
+    private function writeItem($item)
+    {
+        $this->writeSeg('Ide', ['24', 'IDE_REFERENCE']);
+        $this->writeSeg('Imd', ['Z14', 'Z07']);
+        $this->writeSeg('Dtm', ['92', new DateTime('2016-06-15'), 102]);
+        $this->writeSeg('Dtm', ['158', new DateTime('2016-06-15'), 102]);
+        $this->writeSeg('Dtm', ['752', new DateTime('2016-03-10'), 106]);
+        $this->writeSeg('Dtm', ['Z09', new DateTime('2016-03-10'), 602]);
+        $this->writeSeg('Dtm', ['672', new DateTime('2016-03-10'), 802]);
+        $this->writeSeg('Sts', ['7', 'E01']);
+        $this->writeSeg('Sts', ['E01', 'Z43']);
+        $this->writeSeg('Agr', ['11', 'E02']);
+        $this->writeSeg('Agr', ['E03', 'E10']);
+        $this->writeSeg('Loc', ['107', '11YR00000003319J']);
+        $this->writeSeg('Loc', ['237', '11XSTROMMIXER--E']);
+        $this->writeSeg('Loc', ['172', 'DE0002914465200000000000000006517']);
+        $this->writeSeg('Rff', ['Z13', '11010']);
+        $this->writeSeg('Rff', ['TN', 'UE01-26823-166A4DDC4FA']);
+        $this->writeSeg('Cci', ['Z02', 'E01']);
+        $this->writeSeg('Cav', ['H1', '89']);
+        $this->writeSeg('Cci', [null, 'E02']);
+        $this->writeSeg('Cav', ['E02']);
+        $this->writeSeg('Cci', [null, 'E03']);
+        $this->writeSeg('Cav', ['E06']);
+        $this->writeSeg('Cci', [null, 'Z15']);
+
+        $this->writeSeg('Seq', ['Z01']);
+        $this->writeSeg('Rff', ['AVE', 'DE0002914465200000000000000006517']);
+        $this->writeSeg('Qty', ['31', '2293.5', 'KWH']);
+        $this->writeSeg('Cci', ['15', 'Z21']);
+        $this->writeSeg('Cav', ['SLS']);
+        $this->writeSeg('Cci', [null, 'E04']);
+        $this->writeSeg('Cav', ['E06']);
+
+        $this->writeSeg('Seq', ['Z02']);
+        $this->writeSeg('Rff', ['AVE', 'DE0002914465200000000000000006517']);
+        $this->writeSeg('Rff', ['MG', '987654321']);
+        $this->writeSeg('Pia', ['5', '1-1:1.8.0', 'SRW']);
+        $this->writeSeg('Cci', ['11', 'Z33']);
+        $this->writeSeg('Cav', [null, null, '6', '1']);
+
+        $this->writeSeg('Seq', ['Z03']);
+        $this->writeSeg('Rff', ['AVE', 'DE0002914465200000000000000006517']);
+        $this->writeSeg('Cci', [null, 'E13']);
+        $this->writeSeg('Cav', ['AHZ']);
+        $this->writeSeg('Cav', ['Z30', null, '1000564062']);
+        $this->writeSeg('Cav', ['ETZ']);
+        $this->writeSeg('Cav', ['ERZ']);
+        $this->writeSeg('Cci', [null, 'E12']);
+        $this->writeSeg('Cav', ['MMR']);
+
+        $this->writeSeg('Seq', ['Z07']);
+        $this->writeSeg('Rff', ['AVE', 'DE0002914465200000000000000006517']);
+        $this->writeSeg('Rff', ['Z10', '1-1:1.8.0']);
+        $this->writeSeg('Cci', [null, 'Z08']);
+        $this->writeSeg('Cav', ['TA']);
+
+        $this->writeSeg('Seq', ['Z10']);
+        $this->writeSeg('Rff', ['AVE', 'DE0002914465200000000000000006517']);
+        $this->writeSeg('Rff', ['Z10', '1-1:1.8.0']);
+        $this->writeSeg('Cci', ['Z13', 'Z66']);
+
+        $this->writeSeg('Seq', ['Z12']);
+        $this->writeSeg('Qty', ['Z16', '0.0', 'P1']);
+
+        $this->writeSeg('Nad', ['UD', 'Thörmer', 'Kay'], 'fromPerson');
+        $this->writeSeg('Nad', ['DEB', '9904746000004', '293'], 'fromMpCode');
+        $this->writeSeg('Rff', ['Z05', 'JA']);
+
+        $this->writeSeg('Rff', ['AVE', 'DE0002914465200000000000000006517']);
+        $this->writeSeg('Nad', ['DDE', '9904778000006', '293'], 'fromMpCode');
+        $this->writeSeg('Rff', ['Z05', 'JA']);
+
+        $this->writeSeg('Rff', ['AVE', 'DE0002914465200000000000000006517']);
+        $this->writeSeg('Nad', ['DP', 'Kastanienallee', '21', 'Herne', '44652', 'Wanne-Süd'], 'fromAdress');
+        $this->writeSeg('Nad', ['Z04', 'Schmiedestr.', '7', 'Bochum', '44866', 'Günnigfeld'], 'fromAdress');
+        $this->writeSeg('Nad', ['Z05', 'Thörmer', 'Kay', 'Schmiedestr.', '7', 'Bochum', '44866', null, 'Günnigfeld'], 'fromPersonAdress');
+        $this->writeSeg('Rff', ['AVE', 'DE0002914465200000000000000006517']);
+    }
+}
--- tests/Utilmd/U11010/UtilmdU11010Test.php
+++ tests/Utilmd/U11010/UtilmdU11010Test.php
@@ -1,21 +1,21 @@
 <?php
 
-namespace Proengeno\EdiEnergy\Test\Utilmd\U11005;
+namespace Proengeno\EdiEnergy\Test\Utilmd\U11010;
 
 use DateTime;
 use Mockery as m;
 use Proengeno\Edifact\Message\Message;
 use Proengeno\EdiEnergy\Test\TestCase;
-use Proengeno\EdiEnergy\Utilmd\U11005\UtilmdU11005Builder;
+use Proengeno\EdiEnergy\Utilmd\U11010\UtilmdU11010Builder;
 
-class UtilmdU11005Test extends TestCase
+class UtilmdU11010Test extends TestCase
 {
     private $utilmdBuilder;
     private $edifactObject;
 
     public function setUp()
     {
-        $this->utilmdBuilder = new UtilmdU11005Builder('from', 'to', tempnam(sys_get_temp_dir(), 'EdifactTest'));
+        $this->utilmdBuilder = new UtilmdU11010Builder('from', 'to', tempnam(sys_get_temp_dir(), 'EdifactTest'));
     }
 
     public function tearDown()
@@ -29,7 +29,7 @@ class UtilmdU11005Test extends TestCase
     public function it_build_up_the_Message_instance_with_utilmd_13002_mapping()
     {
         $this->assertInstanceOf(Message::class, $this->edifactObject = $this->utilmdBuilder->get());
-        $this->assertEquals('UtilmdU11005', $this->edifactObject->getAdapterName());
+        $this->assertEquals('UtilmdU11010', $this->edifactObject->getAdapterName());
     }
 
     private function makeUtilmdMock(
