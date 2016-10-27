dea8dc9b47f6c4ca7457c102d243e681bfc1e6e7
Nils Refle <n.refle@proengeno.de>
2016-10-27 15:29:39 +0200

    Addet Allocation Rule to Config Class

--- src/Configuration.php
+++ src/Configuration.php
@@ -10,7 +10,27 @@ class Configuration extends BaseConfig
     const GAS = 'gas';
     const ELECTRIC = 'electric';
 
-    private $defaultNamespace = '\Proengeno\EdiEnergy\Segments';
+    protected $segmentNamespace = '\Proengeno\EdiEnergy\Segments';
+    protected $importAllocationRules = [
+        '\Proengeno\EdiEnergy\Utilmd\U11002\UtilmdU11002' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11002/'],
+        '\Proengeno\EdiEnergy\Utilmd\U11003\UtilmdU11003' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11003/'],
+        '\Proengeno\EdiEnergy\Utilmd\U11005\UtilmdU11005' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11005/'],
+        '\Proengeno\EdiEnergy\Utilmd\U11010\UtilmdU11010' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11010/'],
+        '\Proengeno\EdiEnergy\Utilmd\U11016\UtilmdU11016' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11016/'],
+        '\Proengeno\EdiEnergy\Utilmd\U11017\UtilmdU11017' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11017/'],
+        '\Proengeno\EdiEnergy\Utilmd\U11018\UtilmdU11018' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11018/'],
+        '\Proengeno\EdiEnergy\Utilmd\U11022\UtilmdU11022' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11022/'],
+        '\Proengeno\EdiEnergy\Utilmd\U11023\UtilmdU11023' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11023/'],
+        '\Proengeno\EdiEnergy\Utilmd\U11024\UtilmdU11024' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11024/'],
+        '\Proengeno\EdiEnergy\Utilmd\U11036\UtilmdU11036' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11036/'],
+        '\Proengeno\EdiEnergy\Utilmd\U11037\UtilmdU11037' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11037/'],
+        '\Proengeno\EdiEnergy\Utilmd\U11038\UtilmdU11038' => ['UNH' => '/UNH\+(.*?)\+UTILMD\:/', 'RFF' => '/RFF\+Z13\:11038/'],
+        '\Proengeno\EdiEnergy\Orders\O17102\OrdersO17102' => ['UNH' => '/UNH\+(.*?)\+ORDERS\:/', 'RFF' => '/RFF\+Z13\:17102/'],
+        '\Proengeno\EdiEnergy\Orders\O17103\OrdersO17103' => ['UNH' => '/UNH\+(.*?)\+ORDERS\:/', 'RFF' => '/RFF\+Z13\:17103/'],
+        '\Proengeno\EdiEnergy\Remadv\R33001\RemadvR33001' => ['UNH' => '/UNH\+(.*?)\+REMADV\:/', 'RFF' => '/RFF\+Z13\:33001/'],
+        '\Proengeno\EdiEnergy\Mscons\M13002VL\MsconsM13002VL' => ['UNH' => '/UNH\+(.*?)\+MSCONS\:/', 'RFF' => '/RFF\+Z13\:13002/'],
+        '\Proengeno\EdiEnergy\Mscons\M13006VL\MsconsM13006VL' => ['UNB' => '/UNB\+(.*?)\+\+VL/', 'UNH' => '/UNH\+(.*?)\+MSCONS\:/', 'RFF' => '/RFF\+Z13\:13006/'],
+    ];
 
     private $energyType;
     private $outputCharsetConverter;
@@ -27,14 +47,6 @@ class Configuration extends BaseConfig
     public function getEnergyType()
     {
         return $this->energyType ?: self::ELECTRIC;
-    }
-
-    public function getSegmentNamespace()
-    {
-        if (null === parent::getSegmentNamespace()) {
-            $this->setSegmentNamespace($this->defaultNamespace);
-        }
-        return parent::getSegmentNamespace();
     }
 
     public function setOutputCharsetConverter(callable $outputCharsetConverter)
