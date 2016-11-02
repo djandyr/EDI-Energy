399b9c854dc0b784b004bfe69ee8686f043ab08f
Nils Refle <n.refle@proengeno.de>
2016-11-02 09:40:26 +0100

    Addet MsconsM13013/13014

--- src/Mscons/M13013EM/MsconsM13013EM.php
+++ src/Mscons/M13013EM/MsconsM13013EM.php
@@ -1,18 +1,19 @@
 <?php
 
-namespace Proengeno\EdiEnergy\Mscons\M13014EM;
+namespace Proengeno\EdiEnergy\Mscons\M13013EM;
 
 use Proengeno\EdiEnergy\Edifact;
 
-class MsconsM13014EM extends Edifact
+class MsconsM13013EM extends Edifact
 {
     protected static $blueprint = [
         ['name' => 'UNA', 'necessity' => 'O'],
         ['name' => 'UNB'],
         ['name' => 'UNH', 'maxLoops' => 999999, 'necessity' => 'R', 'segments' => [
-            ['name' => 'BGM', 'templates' => ['docCode' => ['Z23'], 'messageCode' => [9]]],
+            ['name' => 'BGM', 'templates' => ['docCode' => ['Z24'], 'messageCode' => [9]]],
             ['name' => 'DTM', 'templates' => ['qualifier' => [137], 'code' => [203]]],
-            ['name' => 'RFF', 'templates' => ['code' => ['Z13'], 'referenz' => ['13014']]],
+            ['name' => 'RFF', 'templates' => ['code' => ['AGI']]],
+            ['name' => 'RFF', 'templates' => ['code' => ['Z13'], 'referenz' => ['13013']]],
             ['name' => 'NAD', 'templates' => ['qualifier' => ['MS']]],
             ['name' => 'CTA', 'necessity' => 'O', 'segments' => [
                 ['name' => 'COM', 'maxLoops' => 5, 'necessity' => 'R']
@@ -21,12 +22,11 @@ class MsconsM13014EM extends Edifact
             ['name' => 'UNS', 'templates' => ['code' => ['D']]],
             ['name' => 'NAD', 'templates' => ['qualifier' => ['DP']]],
             ['name' => 'LOC', 'templates' => ['qualifier' => [172]]],
-            ['name' => 'DTM', 'templates' => ['qualifier' => [9], 'code' => [102]]],
+            ['name' => 'DTM', 'templates' => ['qualifier' => [492], 'code' => [610]]],
             ['name' => 'LIN'],
             ['name' => 'PIA', 'templates' => ['number' => [5], 'articleCode' => ['Z02']]],
             ['name' => 'QTY', 'templates' => ['qualifier' => [79]]],
-            ['name' => 'DTM', 'templates' => ['qualifier' => [163], 'code' => [102]]],
-            ['name' => 'DTM', 'templates' => ['qualifier' => [164], 'code' => [102]]],
+            ['name' => 'DTM', 'templates' => ['qualifier' => [306], 'code' => [102]]],
             ['name' => 'UNT'],
         ]],
         ['name' => 'UNZ']
---
+++ tests/Mscons/M13013EM/MsconsM13013EMTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Proengeno\EdiEnergy\Test\Mscons\M13013EM;
+
+use DateTime;
+use Mockery as m;
+use Proengeno\EdiEnergy\Configuration;
+use Proengeno\Edifact\Message\Message;
+use Proengeno\EdiEnergy\Test\TestCase;
+use Proengeno\EdiEnergy\Interfaces\MsconsVlInterface;
+use Proengeno\EdiEnergy\Mscons\M13013EM\MsconsM13013EMBuilder;
+
+class MsconsM13013EMTest extends TestCase
+{
+    private $msconsBuilder;
+    private $edifactObject;
+
+    public function setUp()
+    {
+        $configuration = new Configuration;
+        $configuration->setExportSender('from');
+        $this->msconsBuilder = new MsconsM13013EMBuilder('to', tempnam(sys_get_temp_dir(), 'EdifactTest'), $configuration);
+    }
+
+    public function tearDown()
+    {
+        if ($this->edifactObject) {
+            @unlink($this->edifactObject->getFilepath());
+        }
+    }
+
+    /** @test */
+    public function it_instanciate_the_correct_class()
+    {
+        $this->assertInstanceOf(MsconsM13013EMBuilder::class, $this->msconsBuilder);
+    }
+
+    /** @test */
+    public function it_build_up_the_Message_instance()
+    {
+        $this->assertInstanceOf(Message::class, $this->edifactObject = $this->msconsBuilder->get());
+    }
+
+    /** @test */
+    public function it_build_up_the_Message_instance_with_mscons_13013_VL_mapping()
+    {
+        $this->assertInstanceOf(Message::class, $this->edifactObject = $this->msconsBuilder->get());
+        $this->assertEquals('MsconsM13013EM', $this->edifactObject->getAdapterName());
+    }
+}
