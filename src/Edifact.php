bae0b35a13d215515ad9105c781fbc0a4e7a121c
Nils Refle <n.refle@proengeno.de>
2016-11-10 16:11:59 +0100

    Moved Moethod to Core Package

--- src/Edifact.php
+++ src/Edifact.php
@@ -3,19 +3,10 @@
 namespace Proengeno\EdiEnergy;
 
 use Proengeno\EdiEnergy\Configuration;
-use Proengeno\Edifact\Message\EdifactFile;
 use Proengeno\Edifact\Templates\AbstractMessage;
 
 abstract class Edifact extends AbstractMessage
 {
-    public static function fromString($string, Configuration $configuration = null)
-    {
-        $file = new EdifactFile('php://temp', 'w+');
-        $file->writeAndRewind($string);
-
-        return new static($file, $configuration);
-    }
-
     protected function getSegmentObject($segLine)
     {
         return parent::getSegmentObject(
