6ca2a6d8ae23d7f9114b7f0b955491a4a39c09b2
Nils Refle <n.refle@proengeno.de>
2018-01-09 15:44:23 +0100

    MelMalo anfragen geaddet

---
+++ src/Utilmd/U11183/UtilmdU11183Builder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Proengeno\EdiEnergy\Utilmd\U11183;
+
+use DateTime;
+use Proengeno\EdiEnergy\Utilmd\UtilmdBuilder;
+
+class UtilmdU11183Builder extends UtilmdBuilder
+{
+    const CHECK_DIGIT = 11001;
+
+    public function getDescriptionPath()
+    {
+        return __DIR__ . '/UtilmdU11183Description.php';
+    }
+
+    protected function writeMessage($items)
+    {
+        $this->writeSeg('Unh', [
+            $this->unbReference(),
+            $this->description->get('versions.message_type-bl'),
+            $this->description->get('versions.version_number'),
+            $this->description->get('versions.release_number'),
+            $this->description->get('versions.organisation'),
+            $this->description->get('versions.organisation_code'),
+        ]);
+        $this->writeSeg('Bgm', ['Z35', $this->unbReference()]);
+        $this->writeSeg('Dtm', [137, new DateTime, 203]);
+        $this->writeSeg('Nad', ['MS', $this->from, $this->getNadQualifier($this->from)], 'fromMpCode');
+        $this->writeSeg('Cta', ['IC', 'Herr Geerdes']);
+        $this->writeSeg('Com', ['04958 91570-10', 'TE']);
+        $this->writeSeg('Com', ['j.geerdes@proengeno.de', 'EM']);
+        $this->writeSeg('Nad', ['MR', $this->to, $this->getNadQualifier($this->to)], 'fromMpCode');
+
+        foreach ($items as $item) {
+            $this->writeItem($item);
+        }
+
+        $this->writeSeg('Unt', [$this->unhCount() + 1, $this->unbReference()]);
+    }
+
+    private function writeItem($item)
+    {
+        $this->writeSeg('Ide', ['24', $item->getIdeRef()]);
+        $this->writeSeg('Imd', ['Z14', 'Z07']);
+        $this->writeSeg('Sts', ['7', 'ZJ7']);
+        $this->writeSeg('Loc', ['172', $item->getMeterpoint()]);
+
+        $this->writeSeg('Seq', ['Z01']);
+        $this->writeSeg('Rff', ['AVE', $item->getMeterpoint()]);
+        $this->writeSeg('Cci', ['Z15', 'Z30']);
+    }
+}
---
+++ src/Utilmd/U11183/UtilmdU11183Description.php
@@ -0,0 +1,176 @@
+<?php
+
+return [
+    'name' => 'Utilmd11183',
+    'check_digit' => 11183,
+    'versions' => [
+        'syntax_id' => 'UNOC',
+        'syntax_version' => 3,
+        'version_number' => 'D',
+        'organisation' => 'UN',
+        'message_subtype' => '',
+        'release_number' => '11A',
+        'message_type' => 'UTILMD',
+        'organisation_code' => '5.1g',
+    ],
+    'validation' => [
+        ['name' => 'UNA', 'necessity' => 'O'],
+        ['name' => 'UNB'],
+        ['name' => 'LOOP', 'maxLoops' => 999999, 'necessity' => 'R', 'segments' => [
+            ['name' => 'UNH'],
+            ['name' => 'BGM', 'templates' => ['docCode' => ['E01']]],
+            ['name' => 'DTM', 'templates' => ['qualifier' => [137], 'code' => [203]]],
+            ['name' => 'NAD', 'templates' => ['qualifier' => ['MS', 'MR']]],
+            ['name' => 'LOOP', 'maxLoops' => 999999, 'necessity' => 'O', 'segments' => [
+                ['name' => 'CTA', 'necessity' => 'O'],
+                ['name' => 'LOOP', 'maxLoops' => 5, 'necessity' => 'O', 'segments' => [
+                    ['name' => 'COM'],
+                ]],
+            ]],
+            ['name' => 'NAD', 'templates' => ['qualifier' => ['MS', 'MR']]],
+            ['name' => 'IDE', 'templates' => ['qualifier' => ['24']]],
+            ['name' => 'IMD', 'templates' => ['code' => ['Z14'], 'qualifier' => ['Z07', 'Z08']]],
+            ['name' => 'DTM', 'templates' => ['qualifier' => [92], 'code' => [102]]],
+            ['name' => 'DTM', 'necessity' => 'O', 'templates' => ['qualifier' => [93], 'code' => [102]]],
+            ['name' => 'DTM', 'necessity' => 'O', 'templates' => ['qualifier' => [155], 'code' => [102]]],
+            ['name' => 'DTM', 'necessity' => 'O', 'templates' => ['qualifier' => [752], 'code' => [104, 106]]],
+            ['name' => 'DTM', 'necessity' => 'O', 'templates' => ['qualifier' => ['Z09'], 'code' => [602]]],
+            ['name' => 'DTM', 'necessity' => 'O', 'templates' => ['qualifier' => ['672'], 'code' => [802]]],
+            ['name' => 'DTM', 'necessity' => 'O', 'templates' => ['qualifier' => [158], 'code' => [102]]],
+            ['name' => 'DTM', 'necessity' => 'O', 'templates' => ['qualifier' => [158], 'code' => [102]]],
+            ['name' => 'STS', 'templates' => ['category' => [7], 'reason' => ['E01', 'E02', 'E03', 'E04', 'ZD2']]],
+            ['name' => 'STS', 'templates' => ['category' => ['E01'], 'reason' => ['E15', 'Z01', 'Z43']]],
+            ['name' => 'AGR', 'templates' => ['qualifier' => [11], 'type' => ['E02']]],
+            ['name' => 'AGR', 'templates' => ['qualifier' => ['E03'], 'type' => ['E10']]],
+            ['name' => 'LOC', 'necessity' => 'O', 'templates' => ['qualifier' => ['Z02', 'Z03']]],
+            ['name' => 'LOC', 'templates' => ['qualifier' => ['107']]],
+            ['name' => 'LOC', 'necessity' => 'O', 'templates' => ['qualifier' => ['Z07']]],
+            ['name' => 'LOC', 'templates' => ['qualifier' => ['237']]],
+            ['name' => 'LOC', 'templates' => ['qualifier' => ['172']]],
+            ['name' => 'RFF', 'templates' => ['code' => ['Z13'], 'referenz' => ['11002']]],
+            ['name' => 'RFF', 'templates' => ['code' => ['TN']]],
+            ['name' => 'LOOP', 'maxLoops' => 20, 'necessity' => 'O', 'segments' => [
+                ['name' => 'CCI', 'necessity' => 'O'],
+                ['name' => 'CAV', 'necessity' => 'O'],
+            ]],
+
+            ['name' => 'SEQ', 'templates' => ['code' => ['Z01']]],
+            ['name' => 'RFF', 'templates' => ['code' => ['AVE']]],
+            ['name' => 'LOOP', 'maxLoops' => 20, 'necessity' => 'O', 'segments' => [
+                ['name' => 'QTY', 'necessity' => 'O'],
+            ]],
+            ['name' => 'LOOP', 'maxLoops' => 20, 'necessity' => 'O', 'segments' => [
+                ['name' => 'CCI', 'necessity' => 'O'],
+                ['name' => 'CAV', 'necessity' => 'O'],
+            ]],
+
+            ['name' => 'LOOP', 'maxLoops' => 1, 'necessity' => 'O', 'segments' => [
+                ['name' => 'SEQ', 'templates' => ['code' => ['Z02']]],
+                ['name' => 'RFF', 'templates' => ['code' => ['AVE']]],
+                ['name' => 'RFF', 'necessity' => 'O', 'templatUtilmdU11002es' => ['code' => ['MG', 'Z11']]],
+                ['name' => 'PIA'],
+                ['name' => 'LOOP', 'maxLoops' => 20, 'necessity' => 'O', 'segments' => [
+                    ['name' => 'CCI', 'necessity' => 'O'],
+                    ['name' => 'CAV', 'necessity' => 'O'],
+                ]],
+            ]],
+
+            ['name' => 'LOOP', 'maxLoops' => 1, 'necessity' => 'O', 'segments' => [
+                ['name' => 'SEQ', 'templates' => ['code' => ['Z07']]],
+                ['name' => 'RFF', 'templates' => ['code' => ['AVE']]],
+                ['name' => 'RFF', 'necessity' => 'O', 'templates' => ['code' => ['Z10']]],
+                ['name' => 'LOOP', 'maxLoops' => 20, 'necessity' => 'O', 'segments' => [
+                    ['name' => 'CCI', 'necessity' => 'O'],
+                    ['name' => 'CAV', 'necessity' => 'O'],
+                ]],
+            ]],
+
+            ['name' => 'LOOP', 'maxLoops' => 1, 'necessity' => 'O', 'segments' => [
+                ['name' => 'SEQ', 'templates' => ['code' => ['Z10']]],
+                ['name' => 'RFF', 'templates' => ['code' => ['AVE']]],
+                ['name' => 'RFF', 'necessity' => 'O', 'templates' => ['code' => ['Z10']]],
+                ['name' => 'CCI'],
+            ]],
+
+            ['name' => 'LOOP', 'maxLoops' => 1, 'necessity' => 'O', 'segments' => [
+                ['name' => 'SEQ', 'templates' => ['code' => ['Z12']]],
+                ['name' => 'QTY'],
+            ]],
+
+            ['name' => 'LOOP', 'maxLoops' => 1, 'necessity' => 'O', 'segments' => [
+                ['name' => 'SEQ', 'templates' => ['code' => ['Z03']]],
+                ['name' => 'RFF', 'templates' => ['code' => ['AVE']]],
+                ['name' => 'RFF', 'necessity' => 'O', 'templates' => ['code' => ['Z14']]],
+                ['name' => 'LOOP', 'maxLoops' => 20, 'necessity' => 'O', 'segments' => [
+                    ['name' => 'CCI', 'necessity' => 'O'],
+                    ['name' => 'CAV', 'necessity' => 'O'],
+                ]],
+            ]],
+
+            ['name' => 'LOOP', 'maxLoops' => 1, 'necessity' => 'O', 'segments' => [
+                ['name' => 'SEQ', 'templates' => ['code' => ['Z04']]],
+                ['name' => 'RFF', 'templates' => ['code' => ['MG']]],
+                ['name' => 'RFF', 'necessity' => 'O', 'templates' => ['code' => ['Z14']]],
+                ['name' => 'CCI'],
+                ['name' => 'CAV'],
+                ['name' => 'CAV'],
+            ]],
+
+            ['name' => 'LOOP', 'maxLoops' => 1, 'necessity' => 'O', 'segments' => [
+                ['name' => 'SEQ', 'templates' => ['code' => ['Z09']]],
+                ['name' => 'RFF', 'templates' => ['code' => ['MG']]],
+                ['name' => 'CCI'],
+                ['name' => 'CAV'],
+                ['name' => 'CAV'],
+            ]],
+
+            ['name' => 'LOOP', 'maxLoops' => 1, 'necessity' => 'O', 'segments' => [
+                ['name' => 'SEQ', 'templates' => ['code' => ['Z05']]],
+                ['name' => 'RFF', 'templates' => ['code' => ['MG']]],
+                ['name' => 'CCI'],
+                ['name' => 'CAV'],
+                ['name' => 'CAV'],
+            ]],
+
+            ['name' => 'LOOP', 'maxLoops' => 1, 'necessity' => 'O', 'segments' => [
+                ['name' => 'SEQ', 'templates' => ['code' => ['Z06']]],
+                ['name' => 'RFF', 'templates' => ['code' => ['MG']]],
+                ['name' => 'CCI'],
+                ['name' => 'CAV'],
+                ['name' => 'CAV'],
+            ]],
+
+            ['name' => 'LOOP', 'maxLoops' => 1, 'necessity' => 'O', 'segments' => [
+                ['name' => 'SEQ', 'templates' => ['code' => ['Z13']]],
+                ['name' => 'RFF', 'templates' => ['code' => ['Z15']]],
+                ['name' => 'CCI'],
+                ['name' => 'CAV'],
+            ]],
+
+            ['name' => 'LOOP', 'maxLoops' => 1, 'necessity' => 'O', 'segments' => [
+                ['name' => 'SEQ', 'templates' => ['code' => ['Z14']]],
+                ['name' => 'RFF', 'templates' => ['code' => ['Z14']]],
+                ['name' => 'RFF', 'templates' => ['code' => ['MG']]],
+                ['name' => 'CCI'],
+                ['name' => 'CAV'],
+            ]],
+            ['name' => 'NAD', 'necessity' => 'O', 'templates' => ['qualifier' => ['UD']]],
+            ['name' => 'RFF', 'necessity' => 'O', 'templates' => ['code' => ['AVC']]],
+            ['name' => 'NAD', 'templates' => ['qualifier' => ['Z04']]],
+            ['name' => 'NAD', 'necessity' => 'O', 'templates' => ['qualifier' => ['DEB']]],
+            ['name' => 'RFF', 'templates' => ['code' => ['Z05']]],
+            ['name' => 'RFF', 'templates' => ['code' => ['AVE']]],
+            ['name' => 'RFF', 'necessity' => 'O', 'templates' => ['code' => ['MG']]],
+            ['name' => 'NAD', 'necessity' => 'O', 'templates' => ['qualifier' => ['DDE']]],
+            ['name' => 'RFF', 'templates' => ['code' => ['Z05']]],
+            ['name' => 'RFF', 'templates' => ['code' => ['AVE']]],
+            ['name' => 'RFF', 'necessity' => 'O', 'templates' => ['code' => ['MG']]],
+            ['name' => 'NAD', 'necessity' => 'O', 'templates' => ['qualifier' => ['DP']]],
+            ['name' => 'NAD', 'necessity' => 'O', 'templates' => ['qualifier' => ['Z05']]],
+            ['name' => 'RFF', 'necessity' => 'O', 'templates' => ['code' => ['MG']]],
+            ['name' => 'RFF', 'templates' => ['code' => ['AVE']]],
+            ['name' => 'UNT'],
+        ]],
+        ['name' => 'UNZ']
+    ]
+];
